8985f4adbbf3f829f3541783cd18a22642ca6168
sameer arshad <arshadsameer820@gmail.com>
2024-11-05 00:45:53 +0500

    types of operators

---
+++ Operators and Conditionals/index.js
@@ -0,0 +1,20 @@
+
+let a = 2;
+let b = 3;
+
+// power of
+console.log(a**b);
+
+// comparison 
+console.log('5' == 5);   // loose
+console.log('5' === 5);  // strict
+
+// ternary operator
+let age = 21;
+let vote = (age > 18) ? "I Can Vote" : "I cannot Vote";
+console.log(vote);
+
+
+// Bitwise operator
+console.log(5 << 3);    // left shift
+console.log(10 >> 1);   // right shift
